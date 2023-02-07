b239fb09687bdc45922f2e7fa32d23e39101950b
Sunit Mal <110469858+sunit-mal@users.noreply.github.com>
2023-02-08 03:04:41 +0530

    Add files via upload

---
+++ Yourtv/urls.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from django.urls import path,include
+from TVApp import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',views.homepage,name='homepage'),
+    path('home/',views.homepage,name='homepage'),
+    path('GoodByeMovie/',views.goodbye,name='goodbye'),
+    path('Charlie/',views.charlie,name='charlie'),
+    path('chup/',views.chup),
+    path('freddy/',views.freddy),
+    path('ghostStory/',views.ghoststory),
+    path('about/',views.about,name='about'),
+    path('login/',views.user_login,name='userlogin'),
+    path('signup/',views.uesr_signup,name='usersignup'),
+    path('logout/', views.user_logout, name='userlogout'),
+]
